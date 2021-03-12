43e2f904bc1a5191d3987fb1c0c3b7e2e061f634
Andres <andresjml@gmail.com>
2021-03-11 19:32:02 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function add() {
+    
+}
+function subtract() {
+    
+}
+function multiply() {
+    
+}
+function divide() {
+    
+}
+let a=5, b=6;
+function add(a, b) {
+    return (a + b);
+}
+function subtract(a, b) {
+    return (a - b);
+}
+function multiply(a, b) {
+    return (a * b);
+}
+function divide(a, b) {
+    return (a / b);
+}
+
+let n = 38;
+function increment(n) {
+    n++;
+    return n;
+}
+function decrement(n) {
+    n--;
+    return n;
+}
+function makeInt(n) {         
+    return parseInt(n,10);    
+}   
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
+
